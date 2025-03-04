ccb5416bfd2c78115e1f410fcd256b2a12bc47a8
minhieu <tranhieu20034@gmail.com>
2025-03-04 10:07:09 +0700

    staticFile & -install-SCSS

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const path = require("path");
 const express = require("express");
 var morgan = require("morgan");
 const { engine } = require("express-handlebars");
-
 const app = express();
 const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
 //http logger
 app.use(morgan("combined"));
 
