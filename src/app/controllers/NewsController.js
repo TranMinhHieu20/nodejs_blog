9ba7a0f27118f183c340bd3ca98729bfe0222e15
minhieu <tranhieu20034@gmail.com>
2025-03-05 22:40:34 +0700

    routes-controller(MVC)

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,12 @@
+const index = (req, res) => {
+  res.render("news");
+};
+
+const show = (req, res) => {
+  res.send("new details");
+};
+
+module.exports = {
+  index,
+  show,
+};
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
 
 //http logger
@@ -16,9 +18,8 @@ app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "resourses/views"));
 console.log("path:", path.join(__dirname, "resourses/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+//routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
